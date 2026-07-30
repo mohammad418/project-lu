92e05e0283eb7b2625e8e78fb98ffa9c8fbb1959
Mohammad <mnloo573@gmail.com>
2026-07-30 11:41:53 +0330

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainPage from './pages/mainpage';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Garage App</h1> */}
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/mainpage.js
@@ -0,0 +1,11 @@
+function MainPage() {
+    return (
+        <div className="mainpage">
+            <div className="">
+
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
